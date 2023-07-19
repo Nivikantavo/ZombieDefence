ee2ab37b20f708505f25e89e93b4dd2172d4c821
Nivikantavo <63458816+Nivikantavo@users.noreply.github.com>
2023-07-19 16:07:53 +0300

    Redesigned the store hierarchy
    Add some shops scripts
    Redesigned the store hierarchy

---
+++ Assets/Scripts/Shop/ItemView.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ItemView : MonoBehaviour
+{
+    [SerializeField] private Sprite _background;
+    [SerializeField] private Sprite _itemIcon;
+
+    [SerializeField] private Image _backgroundImage;
+    [SerializeField] private Image _itemImage;
+    [SerializeField] private TMP_Text _priceText;
+    [SerializeField] private GameObject _priceLable;
+
+    [SerializeField] private Item _item;
+
+    private Button _sellButton;
+
+    public event UnityAction<Item> ViewClick;
+
+    private void Render()
+    {
+        _backgroundImage.sprite = _background;
+        _itemImage.sprite = _itemIcon;
+        _priceText.text = _item.SellingPrice.ToString();
+
+        if (_item.IsBought)
+        {
+            _priceLable.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        Render();
+        _sellButton.onClick.AddListener(OnSellButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        _sellButton.onClick.RemoveListener(OnSellButtonClick);
+    }
+
+    public void OnSellButtonClick()
+    {
+        ViewClick?.Invoke(_item);
+    }
+}
