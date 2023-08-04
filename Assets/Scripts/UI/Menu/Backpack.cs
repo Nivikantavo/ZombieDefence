9cd4af319124d82bc8b015aac16c25cda078da05
Nivikantavo <63458816+Nivikantavo@users.noreply.github.com>
2023-08-04 21:38:52 +0300

    Now backpack view your inventory
    Добавил замочки на заблокированные сложности
    Добавил отображение инвентаря в разделе backpack

---
+++ Assets/Scripts/UI/Menu/Backpack.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Backpack : MonoBehaviour
+{
+    private const string PushForce = "ForwardDash";
+    private const string SlamForce = "JumpDash";
+    private const string ShieldForce = "Shield";
+
+    [SerializeField] private TMP_Text _granagesCountText;
+    [SerializeField] private TMP_Text _truckHealthText;
+
+    [SerializeField] private Button _pushForce;
+    [SerializeField] private Button _slamForce;
+    [SerializeField] private Button _shieldForce;
+
+    private int _granadesCount;
+    private int _truckHealth;
+    private bool _hasPushForce;
+    private bool _hasSlamForce;
+    private bool _hasShieldForce;
+
+    private IEnumerator Start()
+    {
+        while (SaveSystem.Instance.DataLoaded == false)
+        {
+            yield return new WaitForSecondsRealtime(0.25f);
+        }
+        PlayerData playerData = SaveSystem.Instance.GetData();
+        SetData(playerData);
+    }
+
+    private void SetData(PlayerData playerData)
+    {
+        _granadesCount = playerData.GranadesCount;
+        _truckHealth = playerData.TruckHealth;
+
+        if (playerData.Forces.Contains(PushForce))
+        {
+            _hasPushForce = true;
+        }
+        if (playerData.Forces.Contains(SlamForce))
+        {
+            _hasSlamForce = true;
+        }
+        if (playerData.Forces.Contains(ShieldForce))
+        {
+            _hasShieldForce = true;
+        }
+
+        Renderer();
+    }
+
+    private void Renderer()
+    {
+        _granagesCountText.text = _granadesCount.ToString();
+        _truckHealthText.text = _truckHealth.ToString();
+
+        _pushForce.interactable = _hasPushForce;
+        _slamForce.interactable = _hasSlamForce;
+        _shieldForce.interactable = _hasShieldForce;
+    }
+}
---
+++ Assets/Scripts/UI/Menu/DifficultyButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _difficultyText;
+    [SerializeField] private Image _lockImage;
+    [SerializeField] private Button _button;
+
+    public void Lock()
+    {
+        _difficultyText.gameObject.SetActive(false);
+        _lockImage.gameObject.SetActive(true);
+        _button.interactable = false;
+    }
+
+    public void Unlock()
+    {
+        _difficultyText.gameObject.SetActive(true);
+        _lockImage.gameObject.SetActive(false);
+        _button.interactable = true;
+    }
+}
